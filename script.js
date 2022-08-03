9fbdd4181186e834f20b1227efc2e0e40616ff9a
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-08-03 13:56:00 -0700

    Init commit

---
+++ script.js
@@ -0,0 +1,60 @@
+//Add limit to 100
+
+//Prompt window for a visitor to pick a number
+let pickedNum = prompt("Pick a number 0-100", "");
+
+const container = document.getElementById("container")
+
+//Function to create grids
+function createRows(rowNum) {
+
+    for (let i=0; i<=rowNum; i++) {
+        let divRow = document.createElement('div');
+        container.appendChild(divRow);
+        divRow.classList.add('divRow');
+
+
+        for (let k=0; k<=rowNum; k++) {
+            let divCell = document.createElement('div');
+            divRow.appendChild(divCell);
+            divCell.classList.add('divCell');
+        }
+    }
+}
+
+createRows(pickedNum);
+
+//eventlistner change color of cells
+
+const cells = document.querySelectorAll('.divCell');
+
+cells.forEach((cell) => {
+    cell.addEventListener ('mouseover', () => {
+        cell.classList.add('colorChange');
+    })
+})
+
+//eraser button
+
+const eraserBtn = document.getElementById("eraserBtn");
+
+function erase() {
+    cells.forEach((cell) => {
+        cell.addEventListener ('mouseover', () => {
+            cell.classList.remove('colorChange');
+        })
+    })
+}
+eraserBtn.addEventListener('click', erase);
+
+//reset button
+
+const resetBtn = document.getElementById("resetBtn");
+
+function reset() {
+    cells.forEach((cell) => {
+        cell.classList.remove('colorChange');
+    })
+}
+resetBtn.addEventListener('click', reset);
+
